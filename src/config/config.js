b752cca175a648e56f32193743ec1ceb9bceb0d6
Bachi Allamsetty <bachia@gmail.com>
2019-06-19 10:01:39 +0530

    Removed config.js

--- src/config/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    mongousername : 'root',
-    mongopassword : 'tibil123',
-    mongohostname : 'localhost',
-    mongoport : '27017',
-    mongodb : 'shikshalokam',
-    appPortNo: '3000'
-}
